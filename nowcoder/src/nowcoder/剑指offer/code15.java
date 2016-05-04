fc3223c49a085f7abc16b09607abad644d49ef1e
yangengzhe <emailvip@vip.qq.com>
2016-05-04 09:28:45 +0800

    modify

--- nowcoder/src/nowcoder/剑指offer/code15.java
+++ nowcoder/src/nowcoder/剑指offer/code15.java
@@ -1,11 +1,14 @@
 package nowcoder.剑指offer;
 
+
 /**
  * 调整数组顺序使奇数位于偶数前面 ★★
  * 
  * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分，所有的偶数位于位于数组的后半部分，并保证奇数和奇数，偶数和偶数之间的相对位置不变。
  * 
  * 思路：重点在顺序不发生变化。两个指针，从头开始，第一个指针找偶数，第二个指针在后面找奇数，找到后移动位置（不能交换），直到结束。
+ * 方法二：类似冒泡排序思想，遇到前偶后奇就交换
+ * 方法三：常规方法，创建一个数组，遍历两遍
  * @date 2016年5月3日 下午4:23:48
  * @author yangengzhe
  *
@@ -25,6 +28,21 @@ public class code15 {
                 for(int m=j;m>i;m--)
                     array[m] = array[m-1];
                 array[i] = temp;
+            }
+        }
+    }
+    //类似冒泡
+    void reOrderArray2(int [] array) {
+        for (int i = 0; i < array.length;i++)
+        {
+            for (int j = array.length - 1; j>i;j--)
+            {
+                if (array[j] % 2 == 1 && array[j - 1]%2 == 0) //前偶后奇交换
+                {
+                    int temp = array[j];
+                    array[j] = array[j-1];
+                    array[j-1] = temp;
+                }
             }
         }
     }
