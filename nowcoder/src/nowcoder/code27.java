455d3b178b5492b630134d31c857fc4bd6ebc032
yangengzhe <emailvip@vip.qq.com>
2016-05-16 18:33:21 +0800

    add

---
+++ nowcoder/src/nowcoder/code27.java
@@ -0,0 +1,19 @@
+package nowcoder;
+
+import nowcoder.剑指offer.util.RandomListNode;
+
+/**
+ * 复杂链表的复制
+ * 
+ * 输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，另一个特殊指针指向任意一个节点）。
+ * 
+ * @date 2016年5月16日 下午6:15:22
+ * @author yangengzhe
+ *
+ */
+public class code27 {
+    public RandomListNode Clone(RandomListNode pHead)
+    {
+        
+    }
+}
---
+++ nowcoder/src/nowcoder/剑指offer/util/RandomListNode.java
@@ -0,0 +1,12 @@
+package nowcoder.剑指offer.util;
+
+
+public class RandomListNode {
+    public int label;
+    public RandomListNode next = null;
+    public RandomListNode random = null;
+
+    public RandomListNode(int label) {
+        this.label = label;
+    }
+}
