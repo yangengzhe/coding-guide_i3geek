39b883cd1d46df4187690a820e4f6538570cab87
yangengzhe <emailvip@vip.qq.com>
2016-05-18 11:25:58 +0800

    modify

--- nowcoder/src/nowcoder/code27.java
+++ nowcoder/src/nowcoder/code27.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import nowcoder.剑指offer.util.RandomListNode;
 
 /**
- * 复杂链表的复制
+ * 复杂链表的复制 ★★★★
  * 
  * 输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，另一个特殊指针指向任意一个节点）。
  * 
@@ -19,8 +19,49 @@ import nowcoder.剑指offer.util.RandomListNode;
  */
 public class code27 {
 
+    //方法三
+    public void copy(RandomListNode pHead){
+        while(pHead!=null){
+            RandomListNode temp = new RandomListNode(pHead.label);
+            temp.next = pHead.next;
+            pHead.next = temp;
+            pHead = temp.next;
+        }
+    }
+    public void copyRandom(RandomListNode pHead){
+        while(pHead!=null){
+            RandomListNode node = pHead.next;
+            if(pHead.random!=null)
+                node.random = pHead.random.next;
+            else
+                node.random = null;
+            pHead = node.next;
+        }
+    }
+    public RandomListNode divide(RandomListNode pHead){
+        RandomListNode result = pHead.next;
+        RandomListNode _result = result;
+        while(pHead!=null){
+            pHead.next = result.next;
+            pHead = pHead.next;
+            if(pHead!=null){
+                result.next = pHead.next;
+                result = result.next;
+            }
+            else{
+                result.next = null;
+            }
+        }
+        return _result;
+    }
+    public RandomListNode Clone(RandomListNode pHead){
+        if(pHead == null) return null;
+        copy(pHead);
+        copyRandom(pHead);
+        return divide(pHead);
+    }
   //方法二
-    public RandomListNode Clone(RandomListNode pHead)
+    public RandomListNode Clone2(RandomListNode pHead)
     {
         if(pHead == null) return null;
         HashMap<RandomListNode, RandomListNode> map = new HashMap<RandomListNode,RandomListNode>();
